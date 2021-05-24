6be824b9fc9a52239a49f6ec85ca3729d76c16a8
dkhampshire <84059576+dkhampshire@users.noreply.github.com>
2021-05-23 23:56:57 -0500

    Main pages commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
+/* mobile menu*/
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('nav-ul');
 
 hamburger.addEventListener('click', () =>{
 	navUL.classList.toggle('show');
+});
+/*contact popup*/
+document.getElementById('contact').addEventListener('click',
+	function() {
+		document.querySelector('.bg-modal').style.display = 'flex';	
+	});
+/*cancel button*/
+document.querySelector('.close').addEventListener('click', function() {
+	document.querySelector('.bg-modal').style.display= 'none';
 });
